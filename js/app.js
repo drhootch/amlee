a3da4a90076322cf2a393f26cd751a4e5b7e675e
Ishak Amri <amri.isk@gmail.com>
2022-05-19 03:39:47 +0100

    -Animations added

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
 var T2S;
 
-if("speechSynthesis" in window || speechSynthesis){ // Checking If speechSynthesis Is Supported.
+if ("speechSynthesis" in window || speechSynthesis) { // Checking If speechSynthesis Is Supported.
     T2S = window.speechSynthesis || speechSynthesis; // Storing speechSynthesis API as variable - T2S
-     // To Speak The Utterance
-    window.onbeforeunload = function(){
+    // To Speak The Utterance
+    window.onbeforeunload = function () {
         T2S.cancel(); // To Stop Speaking If the Page Is Closed.
     }
 }
 
 document.addEventListener('alpine:init', () => {
     Alpine.data('game', function () {
+
         return {
             async init() {
-                if(this.generatedLetters === ''){
+                if (this.generatedLetters === '') {
                     this.nextWordClicked()
                 }
+                Alpine.nextTick(() => this.animate())
+                document.querySelector('#photo img').onload = () => this.loadingImage = false;
             },
             currentWordIndex: this.$persist(0),
             get correctWord() { return this.answers?.[this.currentWordIndex]?.ar ?? '' },
@@ -23,13 +26,12 @@ document.addEventListener('alpine:init', () => {
             generatedLetters: this.$persist(''),
             generateLetters() {
                 let tmp = this.correctWord;
-                for(let i = 0, l=this.lettersCount-tmp.length; i < l; i++)
-                {
+                for (let i = 0, l = this.lettersCount - tmp.length; i < l; i++) {
                     let rdmL = '';
                     while (rdmL === "" || tmp.includes(rdmL)) {
-                        rdmL = this.letters[Math.floor(Math.random()*this.letters.length)]
+                        rdmL = this.letters[Math.floor(Math.random() * this.letters.length)]
                     }
-                    tmp+=rdmL;
+                    tmp += rdmL;
                 }
                 this.generatedLetters = this.shuffle(tmp)
             },
@@ -42,54 +44,86 @@ document.addEventListener('alpine:init', () => {
                 this.currentTry += letter
             },
             nextWordClicked() {
-                this.currentTry = '';
-                this.currentWordIndex++;
-                if (this.currentWordIndex >= this.answers.length) {
-                    this.currentWordIndex = 0;
-                }
-                this.generateLetters()
-                this.generateImage()
+                this.animating = true;
+                this.animate(false).then(() => {
+                    this.currentTry = '';
+                    this.currentWordIndex++;
+                    if (this.currentWordIndex >= this.answers.length) {
+                        this.currentWordIndex = 0;
+                    }
+                    this.generateLetters()
+                    this.generateImage()
+                    Alpine.nextTick(() => this.animate().then(() => this.animating = false))
+                })
             },
+            animating: false,
             speaking: false,
             readWord() {
                 var msg = new SpeechSynthesisUtterance()
-                msg.lang='ar'
-                msg.text=this.correctWord
-                msg.onend = (event)=> {
+                msg.lang = 'ar'
+                msg.text = this.correctWord
+                msg.onend = (event) => {
                     this.speaking = false;
                 }
                 this.speaking = true;
                 T2S.speak(msg);
             },
             get openModal() {
-                return this.correctWord.split('').every(r=> this.currentTry.includes(r))
+                return !this.animating && this.correctWord.split('').every(r => this.currentTry.includes(r))
             },
             async generateImage() {
-                this.currentImage = null;
+                this.loadingImage = true;
                 this.currentImage = await this.fetchImage(this.answers?.[this.currentWordIndex]?.en)
             },
+            loadingImage: false,
             //Options
             lettersCount: 8,
-            answers: [{ar:'ذئب', en:'wolf'}, {ar:'برتقال', en:'orange'}, {ar:'فأرة', en:'mice'}],
+            answers: [{ ar: 'ذئب', en: 'wolf' }, { ar: 'برتقال', en: 'orange' }, { ar: 'فأرة', en: 'mice' }],
             //Static
             letters: [
-                'ى','ئ','ؤ','آ','إ','أ','ء',
-                'ض','ص','ث','ق','ف','غ','ع','ه','خ','ح','ج',
-                'ش','س','ي','ب','ل','ا','ت','ن','م','ك','ط',
-                'ذ','ر','ة','و','ز','ظ','د',
+                'ى', 'ئ', 'ؤ', 'آ', 'إ', 'أ', 'ء',
+                'ض', 'ص', 'ث', 'ق', 'ف', 'غ', 'ع', 'ه', 'خ', 'ح', 'ج',
+                'ش', 'س', 'ي', 'ب', 'ل', 'ا', 'ت', 'ن', 'م', 'ك', 'ط',
+                'ذ', 'ر', 'ة', 'و', 'ز', 'ظ', 'د',
             ],
             //Tools
             //API
             async fetchImage(word) {
-                let data = await fetch("https://pixabay.com/api/?key=27297690-f114455757e5b498e86889b33&image_type=photo&editors_choice=1&q="+word);
+                let data = await fetch("https://pixabay.com/api/?key=27297690-f114455757e5b498e86889b33&image_type=photo&editors_choice=1&q=" + word);
                 data = await data.json()
-                let image = data.hits[Math.floor(Math.random()*data.hits.length)].webformatURL
+                let image = data.hits[Math.floor(Math.random() * data.hits.length)].webformatURL
                 return image
             },
             //Strings
-            shuffle: str => [...str].sort(()=>Math.random()-.5).join(''),
+            shuffle: str => [...str].sort(() => Math.random() - .5).join(''),
             isLetter(str) {
                 return str.length === 1 && str.match(/[\u0600-\u06FF]/)?.[0];
+            },
+            //Animations
+            animate(animateIn=true) {
+                return new Promise((resolve, reject) => {
+                    var tl = anime.timeline({
+                        direction: animateIn ? 'normal' : 'reverse',
+                    });
+                    tl.add({
+                        targets: '#photo',
+                        scale: [0, '100%'],
+                        easing: 'easeOutElastic(1, .4)'
+                    }).add({
+                        targets: '#letters>button',
+                        scale: [0, '100%'],
+                        delay: function (el, i, l) {
+                            return i * 100;
+                        }
+                    }, '-=600').add({
+                        targets: '#slots>div',
+                        scale: [0, '100%'],
+                        delay: function (el, i, l) {
+                            return i * 100;
+                        }
+                    }, '-=600')
+                    tl.finished.then(resolve);
+                })
             },
         }
     })
